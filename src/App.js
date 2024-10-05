c407bb429688a0efa9f18dcb70d5286d9f076889
Adriano <adriano.ocf@hotmai.com>
2024-10-05 11:38:15 -0300

    commit apos deploy 1o

--- src/App.js
+++ src/App.js
@@ -3,6 +3,32 @@ import RoutesApp from './routes';
 import AuthProvider from './contexts/auth';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css"
+import { CgVercel } from 'react-icons/cg';
+
+// nome do site em url https://chamados-nine.vercel.app/
+
+// deploy CgVercel
+
+// 2. Deploy no Vercel
+// O Vercel também é muito fácil de usar e oferece integração perfeita com o GitHub. É ideal para projetos React.
+
+// Passos para deploy no Vercel:
+// Criar uma conta no Vercel: Crie uma conta ou faça login em vercel.com.
+
+// Importar o repositório no Vercel:
+
+// No dashboard, clique em New Project.
+// Conecte sua conta do GitHub e importe o repositório do seu projeto React.
+// Configurar o projeto:
+
+// O Vercel detecta automaticamente que seu projeto é um aplicativo React e configura os scripts de build e publicação.
+// O comando de build geralmente será:
+// bash
+// Copiar código
+// npm run build
+// Deploy automático:
+
+// O Vercel fará o deploy automaticamente. Da mesma forma que no Netlify, ele vai monitorar o repositório e fazer novos deploys após cada push.
 
 function App() {
   return (
